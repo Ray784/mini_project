b4c87684738ebd98766e8e807766c7d32a55bd2d
Ray784 <abhaybhat784@gmail.com>
2019-12-02 15:39:53 +0530

    added outputs from 5 clusters.

--- nlp2.py
+++ nlp2.py
@@ -5,11 +5,9 @@ import os,io,math
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
-from openpyxl import load_workbook
-from sklearn.decomposition import PCA,KernelPCA,FastICA,IncrementalPCA,LatentDirichletAllocation,SparsePCA,TruncatedSVD
+from sklearn.decomposition import PCA,KernelPCA,LatentDirichletAllocation,TruncatedSVD
 from sklearn.metrics.cluster import contingency_matrix
 from sklearn import metrics
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 scaler = StandardScaler(with_mean=True)
 
@@ -27,7 +25,7 @@ def write2d(file, header, arr, actuallabels, y):
 
 files=list()
 cnt=0
-dir=os.path.join(os.getcwd(),'ds141')
+dir=os.path.join(os.getcwd(),'combined')
 for f in os.listdir(dir):
 	if f.endswith(".txt"):
 		cnt+=1
@@ -39,7 +37,7 @@ opt_file = io.open("output.txt", 'w')
 vectorizer = TfidfVectorizer(analyzer='word',input='filename',token_pattern='[^\n\,\.\s\!\'\?\"\:\;\`\~\)\(\-0123456789][^\n\?\,\.\s\!\'\"\;\:\`\~\)\(\-0123456789]+')
 X=vectorizer.fit_transform(file_iter,'ds141')        
 X=X.toarray()
-actuallabels=[0]*47+[1]*47+[2]*47
+actuallabels=[0]*21 + [1]*9 + [2]*30 + [3]*49 + [4]*17
 km = KMeans(n_clusters=3, init='k-means++',n_init=20)
 print('data scaled and predicted dimnesions is :'+str(X.shape))
 y=km.fit_predict(X)
@@ -59,7 +57,7 @@ for i in range(cnt-60,cnt+1,10):
 	write2d(opt_file, "PCA"+str(pca.n_components), confusion, actuallabels, y)
 	
 
-for i in range(cnt-60,cnt+1,10):
+for i in range(cnt-60,cnt,10):
 	kpcal = KernelPCA(n_components=i,kernel='linear')
 	kpcl = kpcal.fit_transform(X)
 	km = KMeans(n_clusters=3, init='k-means++',n_init=20)
@@ -68,10 +66,7 @@ for i in range(cnt-60,cnt+1,10):
 	confusion=contingency_matrix(actuallabels,y)
 	write2d(opt_file, "KPCAlin"+str(kpcal.n_components), confusion, actuallabels, y)
 
-	
-
-
-for i in range(cnt-60,cnt+1,10):
+for i in range(cnt-60,cnt,10):
 	kpcap = KernelPCA(n_components=i,kernel='poly')
 	kpcp = kpcap.fit_transform(X)
 	km = KMeans(n_clusters=3, init='k-means++',n_init=20)
@@ -79,9 +74,6 @@ for i in range(cnt-60,cnt+1,10):
 	print("kpcapoly"+str(kpcap.n_components)+"-done")
 	confusion=contingency_matrix(actuallabels,y)
 	write2d(opt_file, "kpcapoly"+str(kpcap.n_components), confusion, actuallabels, y)
-
-
-
 
 for i in range(cnt-60,cnt,10):
 	kpcac = KernelPCA(n_components=i,kernel='cosine')
@@ -92,9 +84,7 @@ for i in range(cnt-60,cnt,10):
 	confusion=contingency_matrix(actuallabels,y)
 	write2d(opt_file, "kpcacos"+str(kpcac.n_components), confusion, actuallabels, y)
 
-
-
-for i in range(cnt-60,cnt+1,10):
+for i in range(cnt-60,cnt,10):
 	kpcas = KernelPCA(n_components=i,kernel='sigmoid')
 	kpcs = kpcas.fit_transform(X)
 	km = KMeans(n_clusters=3, init='k-means++',n_init=20)
@@ -102,36 +92,6 @@ for i in range(cnt-60,cnt+1,10):
 	print("kpcasig"+str(kpcas.n_components)+"-done")
 	confusion=contingency_matrix(actuallabels,y)
 	write2d(opt_file, "kpca-sig"+str(kpcas.n_components), confusion, actuallabels, y)
-
-
-
-"""for i in range(cnt-60,cnt+1,10):
-	fica = FastICA(n_components=i,algorithm='deflation')
-	fic = fica.fit_transform(X)
-	km = KMeans(n_clusters=3, init='k-means++',n_init=20)
-	y=km.fit_predict(fic)
-	print("FICA"+str(fica.n_components)+"-done")
-	confusion=contingency_matrix(actuallabels,y)
-	write2d(opt_file, "FICA"+str(fica.n_components), confusion)
-	print("score: "+str(fowlkes_mallows_score(actuallabels,y)))"""
-
-	
-
-
-
-"""for i in range(cnt-60,cnt+1,10):
-	ipca = IncrementalPCA(n_components=i)
-	ipc = ipca.fit_transform(X)
-	km = KMeans(n_clusters=3, init='k-means++',n_init=20)
-	y=km.fit_predict(ipc)
-	print("IPCA"+str(ipca.n_components)+"-done")
-	confusion=contingency_matrix(actuallabels,y)
-	write2d(opt_file, "IPCA"+str(ipca.n_components), confusion)
-	print("score: "+str(fowlkes_mallows_score(actuallabels,y)))"""
-
-	
-
-
 
 for i in range(cnt-60,cnt+1,10):
 	lda = LatentDirichletAllocation(n_components=i,learning_method='batch')
@@ -142,20 +102,6 @@ for i in range(cnt-60,cnt+1,10):
 	confusion=contingency_matrix(actuallabels,y)
 	write2d(opt_file, "LDA"+str(lda.n_components), confusion, actuallabels, y)
 
-	
-
-
-"""for i in range(cnt-60,cnt+1,10):
-	spca = SparsePCA(n_components=i,method='lars')
-	spc = spca.fit_transform(X)
-	km = KMeans(n_clusters=3, init='k-means++',n_init=20)
-	print("SPCA"+str(spca.n_components)+"-done")
-	confusion=contingency_matrix(actuallabels,y)
-	write2d(opt_file, "SPCA"+str(spca.n_components), confusion)
-	print("score: "+str(fowlkes_mallows_score(actuallabels,y)))"""
-
-	
-
 
 for i in range(cnt-60,cnt+1,10):
 	tsvd = TruncatedSVD(n_components=i)
@@ -165,7 +111,3 @@ for i in range(cnt-60,cnt+1,10):
 	print("TSVD"+str(tsvd.n_components))
 	confusion=contingency_matrix(actuallabels,y)
 	write2d(opt_file, "TSVD"+str(tsvd.n_components), confusion, actuallabels, y)
-
-	
-
-
